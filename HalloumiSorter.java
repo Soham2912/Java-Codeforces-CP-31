05a402a3ef4da8e300b8907595e3e3aa13777d0a
Soham Lahoti <sohamlahoti@gmail.com>
2024-01-21 00:00:01 +0530

    cp-31 Halloumi

---
+++ HalloumiSorter.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class HalloumiSorter {
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int[] sorted = arr.clone();
+            Arrays.sort(sorted);
+
+            if(Arrays.equals(sorted,arr) || k > 1){
+                System.out.println("YES");
+            }else{
+                 System.out.println("NO");
+            }
+
+        }  
+    }
+}
